c7d5f77d25e9b5872c6850e78c0fddf6fa28eed4
fumifumiks <fumifumiks@gmail.com>
2023-07-18 23:15:40 +0900

    set bgcolor transparent in Neovim

--- nvim/lua/core/plugin_config/nightfox.lua
+++ nvim/lua/core/plugin_config/nightfox.lua
@@ -1,2 +1,10 @@
 vim.o.termguicolors = true
-vim.cmd [[ colorscheme duskfox ]]
+vim.cmd([[
+  colorscheme duskfox
+  highlight Normal ctermbg=none guibg=NONE
+  highlight NonText ctermbg=none guibg=NONE
+  highlight LineNr ctermbg=none guibg=NONE
+  highlight Folded ctermbg=none guibg=NONE
+  highlight EndOfBuffer ctermbg=none guibg=NONE
+]])
+
